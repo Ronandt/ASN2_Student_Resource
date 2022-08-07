922ce9b01fd2991a8e703d226d79d67b85e32683
Declan <declanisaackuok@gmail.com>
2022-08-07 21:23:44 +0800

    comment

--- SectionC/main.py
+++ SectionC/main.py
@@ -1,6 +1,3 @@
 
-
+#Please use python 3.8 
 print(sum(map(lambda x : (a:=x.split("|"), int(a[-2]) * ((a[7] == "FullTime") + (int(a[3].split("/")[-1]) > 1995) == 2) * 0.85)[1], open("HRMasterlistB.txt", "r").read().split("\n")[:-1])))
-
-    #
-
